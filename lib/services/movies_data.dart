0a14672341bddc0e25e4b45c92fa351b09b53193
Sheeraz Ali <sheerazali7676@gmail.com>
2023-09-05 18:11:13 +0500

    latest movies displayed

--- lib/services/movies_data.dart
+++ lib/services/movies_data.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
+import 'package:film_fusion/constants/api_key.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import '../model/new_movies_model.dart';
 import '../model/trending_movies_model.dart';
 
 class MovieService {
   static Future<List<Result>> fetchTrendingMovies() async {
     final response = await http.get(
       Uri.parse(
-          'https://api.themoviedb.org/3/trending/movie/day?api_key=47ffa47d8792fe1a414a82a3db71ea2e'),
+          'https://api.themoviedb.org/3/trending/movie/day?api_key=$apiKey'),
     );
 
     if (response.statusCode == 200) {
@@ -15,6 +18,22 @@ class MovieService {
       return trendingMovieModel.results;
     } else {
       throw Exception('Failed to load trending movies');
+    }
+  }
+
+  /// New Movies//
+  static Future<List<Latest>> getNewMovies() async {
+    final response = await http.get(
+      Uri.parse('https://api.themoviedb.org/3/movie/now_playing?api_key=$apiKey'),
+    );
+    debugPrint(response.body);
+    if (response.statusCode == 200) {
+      final lstestMovieModel =
+          LatestMoviesModel.fromJson(json.decode(response.body));
+     
+      return lstestMovieModel.results;
+    } else {
+      throw Exception('Failed to load new movies');
     }
   }
 }
