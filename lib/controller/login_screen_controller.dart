be3dd976bd612b865d657bd658d680cb54fbd83d
Sheeraz Ali <sheerazali7676@gmail.com>
2023-09-15 19:04:43 +0500

    Logn complted wth all functionlity ad frget paswrd

--- lib/controller/login_screen_controller.dart
+++ lib/controller/login_screen_controller.dart
@@ -1,5 +1,122 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:film_fusion/constants/routes.dart';
+import 'package:film_fusion/utils/toast_message.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginScreenController extends GetxController{
-  
+import '../../db/entity/users.dart';
+import '../../db/services/localdb_services.dart';
+
+class LoginController extends GetxController {
+  final isLoading = false.obs;
+  final obscureText = true.obs;
+  final useremailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final formKey = GlobalKey<FormState>();
+
+  String? validateUsername(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Username is required';
+    }
+    return null;
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password is required';
+    }
+    return null;
+  }
+
+  final auth = FirebaseAuth.instance;
+ void login() {
+    if (formKey.currentState!.validate()) {
+      isLoading.value = true;
+
+      auth
+          .signInWithEmailAndPassword(
+            email: useremailController.text.toString(),
+            password: passwordController.text.toString(),
+          )
+          .then((value) async {
+        isLoading.value = false;
+        final emailList = await (await LocalDbService.usersDao).getEmails();
+        final currentUserEmail = auth.currentUser?.email;
+        final userDoc =
+            await firestore.collection('users').doc(auth.currentUser!.email).get();
+        final userData = userDoc.data();
+
+        if (!emailList.contains(currentUserEmail.toString())) {
+          UserProfile _userData = UserProfile.fromFirestore(userData!);
+          await (await LocalDbService.usersDao).insertUserProfile(UserProfile(
+            name: _userData.name,
+            email: _userData.email,
+            username: _userData.username,
+            phone: _userData.phone,
+          ));
+        }
+
+        Get.snackbar(
+          "LOGIN",
+          "Successfully",
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.greenAccent.withOpacity(0.5),
+        );
+        Get.offAllNamed(mainScreen);
+      }).onError((error, stackTrace) {
+        ToastMessage.toastMessage(error.toString());
+        debugPrint(error.toString());
+        isLoading.value = false;
+      });
+    }
+  }
+ 
+
+  void sendPasswordResetEmail(String email) async {
+    try {
+      await auth.sendPasswordResetEmail(email: email);
+      Get.snackbar(
+        "Password Reset Email Sent",
+        "Check your email for password reset instructions.",
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.greenAccent.withOpacity(0.7),
+      );
+    } catch (error) {
+      Get.snackbar(
+        "Error",
+        error.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.redAccent.withOpacity(0.7),
+      );
+    }
+  }
+
+  void forgotPasswordDialog() {
+    Get.defaultDialog(
+      title: "Forgot Password",
+      content: Column(
+        children: [
+          TextFormField(
+            controller: useremailController,
+            validator: validateUsername,
+            decoration: const InputDecoration(labelText: "Enter your email"),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              String email = useremailController.text.trim();
+              if (email.isNotEmpty) {
+                sendPasswordResetEmail(email);
+                Get.back();
+              }
+            },
+            child: const Text("Reset Password"),
+          ),
+        ],
+      ),
+    );
+  }
 }
+
--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,294 +1,176 @@
 import 'package:film_fusion/constants/routes.dart';
-import 'package:film_fusion/utils/toast_message.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'login_with_phone.dart';
+import '../../controller/login_screen_controller.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-enum LoginType { email, phone }
-
-class _LoginScreenState extends State<LoginScreen> {
-  bool isLoading = false;
-  bool _obscureText = true;
-  LoginType _loginType = LoginType.email; // Default to email login
-  // final _formKey = GlobalKey<FormState>();
-  // TextEditingController emailController = TextEditingController();
-  // TextEditingController passwordController = TextEditingController();
-
-  final _formKey = GlobalKey<FormState>();
-  final _useremailController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  String? _validateUsername(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Username is required';
-    }
-    return null;
-  }
-
-  String? _validatePassword(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Password is required';
-    }
-    return null;
-  }
-
-  final _auth = FirebaseAuth.instance;
-
-  void login() {
-    setState(() {
-      isLoading = true;
-    });
-
-    _auth
-        .signInWithEmailAndPassword(
-            email: _useremailController.text.toString(),
-            password: _passwordController.text.toString())
-        .then((value) {
-      setState(() {
-        isLoading = false;
-      });
-
-      Get.snackbar("LOGIN", "successfully",
-          snackPosition: SnackPosition.BOTTOM,
-          backgroundColor: Colors.greenAccent.withOpacity(0.5));
-      Get.offAllNamed(mainScreen);
-    }).onError((error, stackTrace) {
-      ToastMessage.toastMessage(error.toString());
-      setState(() {
-        isLoading = false;
-      });
-    });
-  }
+class LoginScreen extends StatelessWidget {
+  LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    LoginController controller = Get.put(LoginController());
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Container(
         height: double.infinity,
         width: double.infinity,
         decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color.fromRGBO(38, 116, 9, 1),
-            Color.fromRGBO(0, 0, 0, 1),
-          ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(38, 116, 9, 1),
+              Color.fromRGBO(0, 0, 0, 1),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
         ),
         child: Form(
-          key: _formKey,
+          key: controller.formKey, // Access _formKey from controller
           child: Padding(
             padding: EdgeInsets.symmetric(
                 horizontal: Get.width * .1, vertical: Get.height * .06),
-            child: Column(children: [
-              Image.asset(
-                "assets/images/logo.png",
-                height: Get.height * .3,
-              ),
-              TextFormField(
-                controller: _useremailController,
-                validator: _validateUsername,
-                style: const TextStyle(color: Colors.white),
-                decoration: const InputDecoration(
-                  hintText: 'Email',
-                  hintStyle: TextStyle(color: Colors.white),
-                  prefixIcon: Icon(
-                    Icons.person,
-                    color: Colors.white,
-                  ),
-                  alignLabelWithHint: true,
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color:
-                          Colors.white, // Color of the bottom line when focused
-                      width: 2.0, // Thickness of the bottom line when focused
-                    ),
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                        color: Colors.white,
-                        width: 2 // Color of the bottom line when unfocused
-                        ),
-                  ),
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/logo.png",
+                  height: Get.height * .3,
                 ),
-              ),
-              const SizedBox(height: 20.0),
-              TextFormField(
-                controller: _passwordController,
-                validator: _validatePassword,
-                obscureText: _obscureText, // For password fields
-                style: const TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  hintStyle: const TextStyle(
-                    color: Colors.white,
-                  ),
-                  prefixIcon: const Icon(
-                    Icons.lock,
-                    color: Colors.white,
-                  ),
-                  alignLabelWithHint: true,
-                  focusedBorder: const UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color:
-                          Colors.white, // Color of the bottom line when focused
-                      width: 2.0, // Thickness of the bottom line when focused
-                    ),
-                  ),
-                  enabledBorder: const UnderlineInputBorder(
-                    borderSide: BorderSide(
-                        color: Colors.white,
-                        width: 2 // Color of the bottom line when unfocused
-                        ),
-                  ),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                TextFormField(
+                  controller: controller.useremailController,
+                  validator: (value) => controller.validateUsername(value),
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                    hintStyle: TextStyle(color: Colors.white),
+                    prefixIcon: Icon(
+                      Icons.person,
                       color: Colors.white,
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _obscureText = !_obscureText;
-                      });
-                    },
+                    alignLabelWithHint: true,
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors
+                            .white, // Color of the bottom line when focused
+                        width: 2.0, // Thickness of the bottom line when focused
+                      ),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 2 // Color of the bottom line when unfocused
+                          ),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: Get.height * .08,
-              ),
-              GestureDetector(
-                onTap: () {
-                  print("object");
-                  login();
-                },
-                child: Container(
-                  height: 60,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white),
-                  child: Center(
-                      child: isLoading
-                          ? const Center(
-                              child: CircularProgressIndicator(
-                                color: Colors.black,
-                              ),
-                            )
-                          : const Text(
-                              "LOGIN",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 20),
-                            )),
-                ),
-              ),
-              SizedBox(
-                height: Get.height * .005,
-              ),
-              TextButton(
-                onPressed: () => forgetPassowrdDialog(),
-                child: Text(
-                  "Forgot Your Password?",
-                  style: TextStyle(
-                    color: Colors.white.withOpacity(0.8),
+                const SizedBox(height: 20.0),
+                Obx(
+                  () => TextFormField(
+                    controller: controller.passwordController,
+                    validator: (value) => controller.validatePassword(value),
+                    obscureText:
+                        controller.obscureText.value, // For password fields
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      hintStyle: const TextStyle(
+                        color: Colors.white,
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.lock,
+                        color: Colors.white,
+                      ),
+                      alignLabelWithHint: true,
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors
+                              .white, // Color of the bottom line when focused
+                          width:
+                              2.0, // Thickness of the bottom line when focused
+                        ),
+                      ),
+                      enabledBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 2 // Color of the bottom line when unfocused
+                            ),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          controller.obscureText.value
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          controller.obscureText.value =
+                              !controller.obscureText.value;
+                        },
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              // GestureDetector(
-              //   onTap: () {
-              //     Get.to(PhoneNumberScreen());
-              //   },
-              //   child: Container(
-              //     height: 60,
-              //     width: double.infinity,
-              //     decoration: BoxDecoration(
-              //         borderRadius: BorderRadius.circular(10),
-              //         color: Colors.white),
-              //     child: Center(
-              //         child: isLoading
-              //             ? const Center(
-              //                 child: CircularProgressIndicator(
-              //                   color: Colors.black,
-              //                 ),
-              //               )
-              //             : const Text(
-              //                 "LOGIN WITH PHONE",
-              //                 style:
-              //                     TextStyle(color: Colors.black, fontSize: 20),
-              //               )),
-              //   ),
-              // ),
-              SizedBox(
-                height: Get.height * .04,
-              ),
-              TextButton(
-                  onPressed: () => Get.toNamed(signup),
+
+                SizedBox(
+                  height: Get.height * .08,
+                ),
+                // Your existing UI code here
+
+                GestureDetector(
+                  onTap: () {
+                    print("object");
+                    controller
+                        .login(); // Call the login method from the controller
+                  },
+                  child: Container(
+                    height: 60,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white),
+                    child: Obx(
+                      () => Center(
+                          child: controller.isLoading.value
+                              ? const Center(
+                                  child: CircularProgressIndicator(
+                                    color: Colors.black,
+                                  ),
+                                )
+                              : const Text(
+                                  "LOGIN",
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 20),
+                                )),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: Get.height * .005,
+                ),
+                TextButton(
+                  onPressed: () => controller.forgotPasswordDialog(),
                   child: Text(
-                    "SIGN UP",
+                    "Forgot Your Password?",
                     style: TextStyle(
-                        color: Colors.white.withOpacity(0.8), fontSize: 20),
-                  ))
-            ]),
+                      color: Colors.white.withOpacity(0.8),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: Get.height * .04,
+                ),
+                TextButton(
+                    onPressed: () => Get.toNamed(signup),
+                    child: Text(
+                      "SIGN UP",
+                      style: TextStyle(
+                          color: Colors.white.withOpacity(0.8), fontSize: 20),
+                    ))
+              ],
+            ),
           ),
         ),
       ),
-    );
-  }
-
-  Future<void> sendPasswordResetEmail(String email) async {
-    try {
-      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
-      Get.snackbar("Password Reset Email Sent",
-          "Check your email for password reset instructions.",
-          snackPosition: SnackPosition.BOTTOM,
-          backgroundColor: Colors.greenAccent.withOpacity(0.7));
-    } catch (error) {
-      Get.snackbar("Error", error.toString(),
-          snackPosition: SnackPosition.BOTTOM,
-          backgroundColor: Colors.redAccent.withOpacity(0.7));
-    }
-  }
-
-  void forgetPassowrdDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        TextEditingController emailController = TextEditingController();
-        return AlertDialog(
-          title: const Text("Forgot Password"),
-          content: TextField(
-            controller: emailController,
-            decoration: const InputDecoration(
-              labelText: "Enter your email",
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text("Cancel"),
-            ),
-            TextButton(
-              onPressed: () {
-                String email = emailController.text;
-                if (email.isNotEmpty) {
-                  sendPasswordResetEmail(email);
-                  Navigator.of(context).pop();
-                }
-              },
-              child: const Text("Reset Password"),
-            ),
-          ],
-        );
-      },
     );
   }
 }
